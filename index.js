f55d6b14a1f5488946998ba26a7377ce1040f5ed
Tan Zhi Cheng <2302245B@student.tp.edu.sg>
2024-11-04 16:07:50 +0800

    Implemented the backend for add-task

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*
+
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
@@ -6,6 +8,36 @@ var startPage = "index.html";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
+
+const { addResource } = require('./utils/TaskUtil')
+app.post('/add-task', addResource);
+
+app.get('/', (req, res) => {
+res.sendFile(__dirname + "/public/" + startPage);
+})
+server = app.listen(PORT, function () {
+const address = server.address();
+const baseUrl = `http://${address.address == "::" ? 'localhost' :
+address.address}:${address.port}`;
+console.log(`Demo project at: ${baseUrl}`);
+});
+module.exports = {app, server}
+
+*/
+
+
+var express = require('express');
+var bodyParser = require("body-parser");
+var app = express();
+const PORT = process.env.PORT || 5050
+var startPage = "index.html";
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("./public"));
+ 
+const { addTask } = require('./utils/TaskUtil');
+app.post('/add-task', addTask);
+ 
 app.get('/', (req, res) => {
 res.sendFile(__dirname + "/public/" + startPage);
 })
---
+++ utils/TaskUtil.js
@@ -0,0 +1,49 @@
+const { Task } = require('../models/Task');
+const fs = require('fs').promises;
+ 
+async function readJSON(filename) {
+    try {
+        const data = await fs.readFile(filename, 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+ 
+async function writeJSON(object, filename) {
+    try {
+        const allObjects = await readJSON(filename);
+        allObjects.push(object);
+        await fs.writeFile(filename, JSON.stringify(allObjects), 'utf8');
+        return allObjects;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+ 
+async function addTask(req, res) {
+    try {
+        const name = req.body.name;
+        const Subject = req.body.Subject;  // Change from location to subject
+ 
+        // Simple validation for task name and subject
+        if (!name || name.length < 3) {
+            return res.status(400).json({ message: 'Task name must be at least 3 characters long.' });
+        }
+        if (!Subject || Subject.length < 3) {
+            return res.status(400).json({ message: 'Subject must be at least 3 characters long.' });
+        }
+ 
+        const newTask = new Task(name, Subject);  // Create a new task instance
+        const updatedTasks = await writeJSON(newTask, 'utils/tasks.json');  // Update to tasks.json
+        return res.status(201).json(updatedTasks);  // Return the updated tasks
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+ 
+module.exports = {
+    readJSON, writeJSON, addTask  // Export the modified functions
+};
